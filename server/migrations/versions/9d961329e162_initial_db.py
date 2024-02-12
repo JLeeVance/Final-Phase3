8a823aafb05e5e36f1c3768c6f77adc296b73194
J Vance <justinleevance94@gmail.com>
2024-02-12 10:42:02 -0500

    initial commit

---
+++ server/migrations/versions/9d961329e162_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: 9d961329e162
+Revises: 
+Create Date: 2024-02-12 10:42:00.857412
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9d961329e162'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
