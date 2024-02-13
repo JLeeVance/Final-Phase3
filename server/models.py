d5c785b1c97e5e6ffcb9bc3d93a0da4c4e28ad76
J Vance <justinleevance94@gmail.com>
2024-02-13 10:32:16 -0500

    Pokemon Data Obtained

--- server/models.py
+++ server/models.py
@@ -1,2 +1,58 @@
 from config import db
 
+"""Classes"""
+
+# User will be able to Create Trainer or see all Trainers
+# View All Trainers
+    #list of Trainers
+# When a Trainer is selected
+    # the user can add pokemon to their team (max 6) from the list
+# Battle Pokemon
+    #Wild pokemon appears to be fought, one pokemon can fight per battle
+    #fake hp variable used to print the decrementing Hp, but not actually affect the starting HP
+    # If lose (trainer_pokemon Hp is 0, force exit to main screen, choose new trainer), all pokemon removed? Or dead pokemon removed?
+    # If win (wild_opponent HP is 0, exit back to trainer home page to see all pokemon, and view all trainers.)
+
+
+class Trainer(db.Model):
+
+    __tablename__ = "trainers"
+
+    id = db.Column(db.Integer , primary_key = True)
+    name = db.Column(db.String, nullable = False)
+    age = db.Column(db.Integer)
+    motto = db.Column(db.String)
+
+    # pokemons = db.relationship("Pokemon", back_populates = "trainer")
+
+
+class Pokemon(db.Model):
+
+    __tablename__ = "pokemons"
+
+    id = db.Column(db.Integer, primary_key = True)
+
+    name = db.Column(db.String)
+    starting_hp = db.Column(db.Integer)
+    max_hp = db.Column(db.Integer)
+    attack_name = db.Column(db.String)
+    attack_damage = db.Column(db.Integer)
+
+    trainer_id = db.Column(db.Integer, db.ForeignKey("trainers.id"), nullable=True)
+
+    # trainer = db.relationship("Trainer", back_populates = "pokemons")
+    # battles = db.relationship("Battle", back_populates = "pokemons" )
+
+
+class Battle(db.Model):
+
+    __tablename__ = "battles"
+
+    id = db.Column(db.Integer, primary_key = True)
+
+    trainer_pokemon = db.Column(db.Integer , db.ForeignKey('pokemons.id'))
+    wild_pokemon = db.Column(db.Integer , db.ForeignKey('pokemons.id'))
+
+    # pokemons = db.relationship("Pokemon", back_populates = "battles")
+
+    
--- server/seed.py
+++ server/seed.py
@@ -1,8 +1,61 @@
 from config import app
+import requests
+import re
 
 from models import *
 
 if __name__ == "__main__":
   with app.app_context():
-    pass
-    # remove pass and write your seed data
+
+    base_url = "https://api.pokemontcg.io/v2/cards"
+    test_url = "https://api.pokemontcg.io/v2/sets/sv3pt5"
+    params = {
+      "t": 'set.id:sv3pt5',               #working
+      'r': 'set.releaseDate:2023/09/22',  #not working
+      'n': 'name:squirtle'                #working
+    }
+    limit = "&pageSize=151"
+    url = f"{base_url}/?q={params['t']}{limit}"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+      data = response.json()
+    else:
+      print("Error:", response.status_code)
+
+    pokemon = data["data"]
+
+    counter = 0
+    for obj in pokemon:
+
+      if obj["attacks"][0]["damage"] == "" and len(obj["attacks"]) == 1:
+        attack = obj["attacks"][0]
+      elif obj["attacks"][0]["damage"] == "" and len(obj["attacks"]) >= 2:
+        attack = obj["attacks"][1]
+      else:
+        attack = obj["attacks"][0]
+      
+      '''Accounts for damage being 0'''
+
+      if attack["damage"] == "":    
+        attack["damage"] = 20
+
+      '''Removes all non-int characters'''
+      attack["damage"] = str(attack["damage"])
+      attack["damage"] = attack["damage"].replace("×", '').replace('+', '')
+      attack["damage"] = int(attack["damage"])
+
+      print(f"{counter} -- {attack['damage']} \n {attack['name']} -- {obj['hp']}")
+      counter += 1
+
+    #   Pokemon(
+    #     name = obj["name"]
+    #     starting_hp = obj["hp"]
+    #     max_hp = obj["hp"]
+    #     attack_name = attack["name"]
+    #     attack_damage = attack["damage"]
+    #     trainer_id = null
+    #   )
+
+
+ 
